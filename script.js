46f2cd7e4cd05f27197811e24710365fefaab8f7
Atukholko <43937413+Atukholko@users.noreply.github.com>
2019-10-20 22:53:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -94,27 +94,7 @@ function add_row(table, name, age, mark){
         json = json + obj +']';
     student = JSON.parse(json);
     console.log(student)
-    xhr.open('GET', requestURL, false);
-xhr.onreadystatechange = function()
-{
-    if(xhr.readyState == 4 && xhr.status == 200)
-    {
-        // we convert your JSON into JavaScript object
-        jsonArr = JSON.parse(xhr.responseText);
-
-        // we add new value:
-        jsonArr.push({"name": `${name}`, "age": `${age}`, "mark": `${mark}`});
-
-        // we send with new request the updated JSON file to the server:
-        xhr.open("POST", requestURL, true);
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        // if you want to handle the POST response write (in this case you do not need it):
-        // xhr.onreadystatechange = function(){ /* handle POST response */ };
-        xhr.send(JSON.stringify(jsonArr));
-        // but on this place you have to have a server for write updated JSON to the file
-    }
-};
-xhr.send(null);
+    
     add_row(tb, name, age, mark);
  }
  del = function del_student(){
